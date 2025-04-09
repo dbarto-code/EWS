d11395e7ed05f5c26e7e88c68c6fe8cccc01083c
dbarto-code <daniel.barto@ped.nm.gov>
2025-04-09 14:39:18 -0600

    env switch code

--- Models/placeholder.py
+++ Models/placeholder.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  8 14:15:23 2025
 
-@author: Daniel.Barto
-"""
+import pandas as pd
+import os
+
+#%% 
+def load_directory(local_dir, remote_dir):
+    return local_dir if os.path.exists(local_dir) else remote_dir
+
+local_directory = 'C:\\Users\\Daniel.Barto\\Documents\\GitHub\EWS\\'
+remote_directory = 'OTHER'
+
+main_dir = load_directory(local_directory, remote_directory)
+
 
